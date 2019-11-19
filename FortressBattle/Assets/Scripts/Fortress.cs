1a35074f7019623763fa0792c2783ca1c4813155
pimhoreman@gmail.com <pimhoreman@gmail.com>
2019-11-19 22:09:47 +0100

    -First Commit -Added Fortress.cs -Added Different Scenes for artist and programmer

---
+++ FortressBattle/Assets/Scripts/Fortress.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fortress : MonoBehaviour
+{
+
+	[SerializeField] private int buildingHealth;
+	[SerializeField] private int buildingMaxHealth;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+		buildingHealth = buildingMaxHealth;
+    }
+}
